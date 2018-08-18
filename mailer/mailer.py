06db81461e94f8063193770ec0743c4d31e01692
coprice <collinprice@college.harvard.edu>
2018-08-18 14:52:11 -0400

    remove non-ascii charcters from request messages

--- mailer/mailer.py
+++ mailer/mailer.py
@@ -8,13 +8,18 @@ class Mailer:
         self.user = config.mailer_email
         self.password = config.mailer_password
 
-    def send_request(self, name, email, message, request_type, link, admins, title):
-        server = smtplib.SMTP_SSL('smtp.gmail.com')
-        server.login(self.user, self.password)
-        if message:
-            message = message.replace("’", "'").replace('“', '').replace('”', '')
+    def reset_connection(self):
+        self.server = smtplib.SMTP_SSL('smtp.gmail.com')
+        self.server.login(self.user, self.password)
 
-        msg = "{}: {}\nName: {}\nEmail: {}\nMessage: {}\n\nSign in at the following link to add this user to the {}:\n\n{}".\
+    def send_request(self, name, email, message, request_type, link, admins, title):
+        self.reset_connection()
+
+        if message:
+            message = message.replace("’", "'").replace('“', '"').replace('”', '"')
+            message = ''.join(i for i in message if ord(i) < 128) # remove non-ASCII
+
+        msg = """{}: {}\nName: {}\nEmail: {}\nMessage: {}\n\nSign in at the following link to add this user to the {}:\n\n{}""".\
             format(request_type, title, name, email, message, request_type.lower(), link)
         subject = '{} Request'.format(request_type)
 
@@ -22,23 +27,26 @@ class Mailer:
             format(self.user, ', '.join(admins), subject, msg)
 
         try:
-            server.sendmail(self.user, admins, text)
+            self.server.sendmail(self.user, admins, text)
+            self.server.quit()
             return True
         except:
+            self.server.quit()
             return False
 
     def send_reset(self, email, link):
-        server = smtplib.SMTP_SSL('smtp.gmail.com')
-        server.login(self.user, self.password)
+        self.reset_connection()
 
         msg = "Reset your password at the following link:\n\n{}".format(link)
         text = "From: {}\r\nTo: {}\r\nSubject: [NO REPLY] Password Reset\r\n\r\n{}".\
             format(self.user, email, msg)
 
         try:
-            server.sendmail(self.user, email, text)
+            self.server.sendmail(self.user, email, text)
+            self.server.quit()
             return True
         except:
+            self.server.quit()
             return False
 
 
